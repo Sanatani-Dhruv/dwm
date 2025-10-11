9f37e70954080bb2973057467c648207fd0c32cf
Sanatani-Dhruv <dhruvdds125@gmail.com>
2025-10-11 15:05:30 +0530

    Now Also shows Battery Percent In status bar, done using dwmblocks and upower

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/										/*Update Interval*/	/*Update Signal*/
-	{" Mem: ", 	" free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		0},
+	{" Pow: ", 	"upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep perc | awk '{print $2}'",	30,		0},
+	{"Mem: ", 	" free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		0},
 
 	{"", "date '+%b %d (%a) %I:%M:%S%p '",					1,		0},
 };
